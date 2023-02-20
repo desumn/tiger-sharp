bdfbbf00d336c94873809dc716a7f567538ed08a
desumn <yugamii.43@gmail.com>
2023-02-20 11:26:56 +0100

    Modify lexer further

--- Program.fs
+++ Program.fs
@@ -1,12 +1,17 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
 open Lexer
+open Parser
 open FSharp.Text.Lexing
+open System
 open System.IO
 
 
 let ls = 
     do printf "Super lexer"
-    let lexbuff = LexBuffer<char>.FromString @"""test\n"""
-    let tok = lexer_main "" lexbuff
-    do printfn "%A" tok
-
+    let lexbuff = LexBuffer<char>.FromString @" ""test\n"" "
+    let rec lexing_loop acc =
+        match lexer_main lexbuff with
+        | EOF -> []
+        | _ as tok -> tok :: (lexing_loop acc)
+    let token_list = lexing_loop []
+    do printfn "%A" token_list
